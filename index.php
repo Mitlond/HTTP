7b91dcf80b63bd62dedd31ff049cf5e267aac12d
Mitlond <comp122012016@mail.ru>
2016-10-10 17:55:31 +0300

    HTTP

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<head>
+<title></title>
+</head>
+<body>
+<?php
+//$p=header("http://www.yandex.ru");
+//echo"$p";
+//$d=headers_sent();
+//echo"$d";
+//function nocache(){
+	//header("Expires:Thu,19 Feb 1998 13:24:18 GMT");
+	//header("Last-Modified:".gmdate("D,d M Y H:i:s") ,"GMT");
+	//header("cache-control:no-cache,must-revalidate");
+	//header("cache-control:post-check=0,pre-check=0");
+//header("Pragma:no-cache");
+//}//
+//headers_list();
+if (mail("mr.artem.nikitin1997@mail.ru", "jobs", "privet \n  aga \n  net \n  ugu"))
+echo "Получилось,да?";
+?>
+</body>
+</html>
